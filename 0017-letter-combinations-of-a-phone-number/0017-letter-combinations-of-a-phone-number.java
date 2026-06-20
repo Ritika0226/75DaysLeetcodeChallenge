665887b6c0fc5e1cec3c26112da429c9849db497
Ritika Chaurasiya <shaksichaurasiyasc0207@gmail.com>
2026-06-20 07:48:46 +0530

    Time: 0 ms (100%), Space: 43.5 MB (53.06%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,26 @@
+class Solution {
+    private List<String> result = new ArrayList<>();
+     public List<String> letterCombinations(String digits) {
+        if (digits == null || digits.length() == 0) {
+            return result;
+        }
+        String[] map = {
+            "", "", "abc", "def", "ghi",
+            "jkl", "mno", "pqrs", "tuv", "wxyz"
+        };
+        backtrack(digits, 0, new StringBuilder(), map);
+        return result;
+    }
+    private void backtrack(String digits, int index, StringBuilder current, String[] map) {
+        if (index == digits.length()) {
+            result.add(current.toString());
+            return;
+        }
+        String letters = map[digits.charAt(index) - '0'];
+        for (char ch : letters.toCharArray()) {
+            current.append(ch);
+            backtrack(digits, index + 1, current, map);
+            current.deleteCharAt(current.length() - 1); // backtrack
+        }
+    }
+}
