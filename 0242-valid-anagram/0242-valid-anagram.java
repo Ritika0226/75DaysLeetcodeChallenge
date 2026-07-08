8bb59116326b5f6681f08f68b6ac3faeacdf7f24
Ritika Chaurasiya <shaksichaurasiyasc0207@gmail.com>
2026-07-08 11:20:01 +0530

    Time: 80 ms (5.23%), Space: 44.4 MB (67.08%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,18 +1,19 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+
         if (s.length() != t.length()) {
             return false;
         }
 
-        int[] freq = new int[26];
+        int[] count = new int[26];
 
         for (int i = 0; i < s.length(); i++) {
-            freq[s.charAt(i) - 'a']++;
-            freq[t.charAt(i) - 'a']--;
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
         }
 
-        for (int count : freq) {
-            if (count != 0) {
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) {
                 return false;
             }
         }
