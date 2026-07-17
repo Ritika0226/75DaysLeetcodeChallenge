d4e6628ce40a01be25911e9f05a7c85297bdf178
Ritika Chaurasiya <shaksichaurasiyasc0207@gmail.com>
2026-07-17 18:15:16 +0530

    Time: 0 ms (100%), Space: 52.3 MB (96.8%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    let i = s.length - 1;
+    while (i >= 0 && s[i] === ' ') i--;
+    let len = 0;
+    while (i >= 0 && s[i] !== ' ') {
+        len++;
+        i--;
+    }
+    return len;
+};
