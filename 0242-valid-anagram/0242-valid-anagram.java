f37050d78480e71c46c3838adf2f2e77fc68da67
Ritika Chaurasiya <shaksichaurasiyasc0207@gmail.com>
2026-07-28 21:26:26 +0530

    Time: 4 ms (82.61%), Space: 46.4 MB (34.67%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -4,20 +4,12 @@ class Solution {
         if (s.length() != t.length()) {
             return false;
         }
+        char[] a = s.toCharArray();
+        char[] b = t.toCharArray();
 
-        int[] count = new int[26];
+        Arrays.sort(a);
+        Arrays.sort(b);
 
-        for (int i = 0; i < s.length(); i++) {
-            count[s.charAt(i) - 'a']++;
-            count[t.charAt(i) - 'a']--;
-        }
-
-        for (int i = 0; i < 26; i++) {
-            if (count[i] != 0) {
-                return false;
-            }
-        }
-
-        return true;
+        return Arrays.equals(a, b);
     }
 }
